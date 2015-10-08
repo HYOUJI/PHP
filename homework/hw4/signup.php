8e0c313f5486301400b6a40fe040a02674fe4ce1
YOUJI HWANG <mechuri13@gmail.com>
2015-10-08 23:23:26 +0900

    hw4
    유효성 검사 해야함

--- homework/hw4/signup.php
+++ homework/hw4/signup.php
@@ -2,49 +2,74 @@
 
 <!DOCTYPE html>
 <html>
-<head><title>Attend Job Fair!</title></head>
+<head>
+<title>Attend Job Fair!</title>
+<link href="jobfair.css" type="text/css" rel="stylesheet" />
+</head>
 <body>
+
 	<?php
-	if( isset($_POST["submit"] )) {
+	
 		$name = $_POST["name"];
 		$email = $_POST["email"];
 		$interest = $_POST["interest"];
 		$gender = $_POST["gender"];
 
 		$rlist = "list.txt";
-		$new = true; //신규 등록 여부.
+		$new = true;
+		//신규 등록 여부.
+		$list = explode("\n", file_get_contents($rlist));
 
-		//$list = explode("\n", file_get_contents($rlist));
+		if( empty($name) || empty($email) || empty($interest) || empty($gender) ){
+			echo "<h1>Sorry</h1>";
+			echo "You didn't fill out the form completely. ";
+			echo "<a href='$_SERVER[HTTP_REFERER]'>Try again?</a>";
 
-		foreach ($list as $user) {
-			list( $username, $useremail, $userinterest, $usergender ) = explode(" : ", $user);
-
-			if( $name == trim($username) ){
-				#기존 등록 회원일 때
-				$new = false;
-				if( $email == trim($useremail) ){
-					if( $interest == trim($userinterest) ){
-						if( $gender == trim($usergender) ){
-							echo "<h1>Thanks, Job Seeker !</h1>";
-							echo "<p>You successfully reserved a seat! See you then ^.^<p>";
-
-							echo "<div class="item">Name : </div>";
-							header("Location: signup.php?$_GET['user']");
-							echo "<div class="item">E-mail : </div>";
-							echo "<div class="item">Field of interest : </div>";
-							echo "<div class="item">Gender : </div>";
-
-							echo "<hr>";
-							echo "<h3>Current reservation list </h3>"
-							
-							header("Location: signup.php?=wrong");
-
-						} 
-					} 
-				} 
-			}			
 		}
-	}
+		else {
+			foreach( $list as $user ){
+				list( $username, $useremail, $userinterest, $usergender ) = explode(" : ", $user);
+				
+				#기존회원일 경우
+				
+				if( $name == trim($username) && $email == trim($useremail) && 
+					$interest == trim($userinterest) && $gender == trim($usergender) ){
+					$new = false;
+					echo "이미등록";
+					break;
+				}
+				
+
+			}
+
+		}
+
+		if( $new ){
+			file_put_contents($rlist, $name." : ".$email." : ".$interest." : ".$gender."\n", FILE_APPEND);
+			echo "<h1>Thanks, Job Seeker !</h1>";
+			echo "<p>You successfully reserved a seat! See you then ^.^<p>";
+
+			echo "<div class='item'>Name : </div>";
+			echo $name;
+			echo "<br><div class='item'>E-mail : </div>";
+			echo $email;		
+			echo "<br><div class='item'>Field of interest : </div>";
+			echo $interest;
+			echo "<br><div class='item'>Gender : </div>";
+			echo $gender;
+
+			echo "<hr>";
+			echo "<h3>Current reservation list </h3>";
+
+			$readtxt = fopen("list.txt", "r") or die ("No");
+			while(!feof($readtxt)){
+				echo fgets($readtxt)."<br>";
+			}
+			fclose($myfile);
+
+		}
+
+		
 	?>
 </body>
 </html>
